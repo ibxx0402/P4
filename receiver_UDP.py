39a80eea261d1ecd01959be67dff92f9ba98bd7b
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2025-03-07 16:03:51 +0100

    Update receiver_UDP.py
    Added timeout

--- receiver_UDP.py
+++ receiver_UDP.py
@@ -3,7 +3,6 @@ import numpy as np
 import socket
 import pickle
 
-
 BUFF_SIZE = 65536
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
@@ -16,23 +15,24 @@ sock.bind(socket_address)
 
 
 while True:
+    sock.settimeout(50)
     header_1 = sock.recv(4)
-    header_2 = sock.recv(2)
 
-    if header_1[:1] != b'L' or header_2[:1] != b'S':
-        sock.sendto(b'F', (client_ip, port))
-    else: 
-        sock.sendto(header_1 + header_2, (client_ip, port))
+    if header_1[:1] == b'L':
+        sock.sendto(header_1, (client_ip, port))
         data = b''
         
-        while len(data) < int.from_bytes(header_1[1:], 'big'):
-            data += sock.recv(4096)
+        msg_size = int.from_bytes(header_1[1:], 'big')
         
-        frame = pickle.loads(data)
-        data = b''
-        cv2.imshow('Video Stream', frame)
-
-        # Break loop if 'q' is pressed is necessary for window not closing
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+        try:
+            while len(data) < msg_size:
+                sock.settimeout(0.1)
+                data += sock.recv(1450)
+            
+            frame = pickle.loads(data)
+            cv2.imshow('Video Stream', frame)
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord('q'):
+                break
+        except Exception as e:
+            continue
