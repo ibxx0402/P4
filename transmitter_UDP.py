ffb95bcacdae2582b1727a15b5e209f063d0f10d
Ib Leminen Mohr Nielsen <144134079+ibxx0402@users.noreply.github.com>
2025-03-11 17:12:06 +0100

    working transmitter UDP

--- transmitter_UDP.py
+++ transmitter_UDP.py
@@ -1,63 +1,54 @@
-# Server Side (Raspberry Pi)
 import cv2
-import numpy as np
 import socket
+import math
 import pickle
+import sys
 from picamera2 import Picamera2
 
-BUFF_SIZE = 65536
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, BUFF_SIZE)
-host_name = socket.gethostname()
-host_ip = "192.168.0.111"
-client_ip = "192.168.0.101"
-port = 65432
-address = (host_ip, port)
-s.bind((client_ip, port))
+max_length = 65000
+host = "192.168.0.106"
+port = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 picam2 = Picamera2()
-config = picam2.create_video_configuration(main={"size": (320, 240)}, lores={"size": (320, 240)}, display="main")
+config = picam2.create_video_configuration(main={"size": (1920, 1080)}, lores={"size": (320, 240)}, display="main")
 picam2.configure(config)
-
 
 picam2.start()
 
+#used to calculate the number of packets to be sent
+frame = picam2.capture_array()
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#print(frame.shape)
+retval, buffer = cv2.imencode(".png", frame)
+
+buffer = buffer.tobytes()
+buffer_size = len(buffer)
+
+num_of_packs = 1
+if buffer_size > max_length:
+    num_of_packs = math.ceil(buffer_size/max_length)
+
+frame_info = {"packs":num_of_packs}
+# send the number of packs to be expected
+print("Number of packs:", num_of_packs)
+sock.sendto(pickle.dumps(frame_info), (host, port))
+
 while True:
-    # Capture frame
     frame = picam2.capture_array()
-
-    # Optional: Process frame
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-    # Serialize frame
-    data = pickle.dumps(frame)
-
-    #size chosen because of the MTU size of 1500 bytes
-    frame_size = 1450
-    data_length = len(data) 
-
-    #4 byte header including length of message, 3 byte for data_length
-    header_1 = b'L' + data_length.to_bytes(3, 'big')
-    s.sendto(header_1, address)
-
-    bytes_sent = 0
     
-    # Pre-allocate the view once and reuse
-    data_view = memoryview(bytearray(frame_size))
+    retval, buffer = cv2.imencode(".jpg", frame)
+    # convert to byte array
+    buffer = buffer.tobytes()
 
-    try:
-        while True:
-            s.settimeout(0.1)
-            if s.recv(4) == header_1:
 
-                view = memoryview(data)
-                for i in range(0, data_length, frame_size):
-                    split_data = view[i:min(i+frame_size, data_length)]
-                    #print(len(split_data)) 
-                    bytes_sent += s.sendto(split_data, (host_ip, port))
-                break
-             
-            else:
-                break
-    except socket.timeout:
-        continue
+    view = memoryview(buffer)
+    for i in range(0, buffer_size, max_length):
+        split_data = view[i:min(i+max_length, buffer_size)]
+        sock.sendto(split_data, (host, port))
+
+    # is only used for testing purposes
+    #break
+print("done")
